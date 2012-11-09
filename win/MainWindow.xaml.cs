6064adf025acc300c52eda069fd47bd324caf76f
rkoiva <rkoiva@59f23998-0fa1-4466-a486-63b14a1c9c9a>
2012-11-09 09:14:43 +0000

    Started Windows software
    git-svn-id: https://svn.techfak.uni-bielefeld.de/citec/projects/TactileGlove@345 59f23998-0fa1-4466-a486-63b14a1c9c9a

---
+++ win/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TactileDataglove
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Color goes from RGB (0,50,0) to (0,255,0) to (255,255,0) to (255,0,0)
+
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = Color.FromArgb((byte)slider1.Value, 255,255, 0);
+            PalmUpRF.Fill = mySolidColorBrush;
+        }
+    }
+}
